8008796f5e09082558154514f86c266510249cce
alesnim@gmail.com <alesnim@gmail.com>
2020-11-07 15:32:53 +0300

    work on Cash

--- src/com/javalesson/oop/shop/Cash.java
+++ src/com/javalesson/oop/shop/Cash.java
@@ -1,4 +1,15 @@
 package com.javalesson.oop.shop;
 
 public class Cash {
+    private int money;
+
+    public void addMoney(int m) {
+        money +=m;
+    }
+
+    public int getMoney() {
+        return money;
+    }
+
+
 }
